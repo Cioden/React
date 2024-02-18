395ae54e9f9241eab9be777bcbb8733f39639445
Cioden <dspresado@gmail.com>
2024-02-18 12:06:05 +0800

    initial commit

---
+++ react-info-site/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <div id='root'></div>
+        <script src='index.js' type='text/babel'></script>
+      </body>
+    </div>
+  );
+}
+
+export default App;
