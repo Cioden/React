99d63f2b7e5c60ad73246d65a20d24ffd07934fe
Cioden <110314589+Cioden@users.noreply.github.com>
2024-02-25 16:32:20 +0800

    initial commit

---
+++ react-info-site/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <ul>
+    <li>Thing 1</li>
+    <li>Thing 2</li>
+  </ul>,
+  document.getElementById("root")
+);
